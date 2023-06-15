7d8f16131bfba3f512adb89dac740435c9f08688
Mixxy3k <artur.szandrocha@gmail.com>
2023-06-15 13:16:36 +0200

    SQL Script for databse creation/reset

---
+++ CreateDatabase.sql
@@ -0,0 +1,12 @@
+USE [master]
+GO
+ALTER DATABASE [InventoryManagment] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+GO
+
+/* Query to Drop Database in SQL Server  */
+
+DROP DATABASE [InventoryManagment]
+GO
+
+Create DATABASE [InventoryManagment]
+GO
